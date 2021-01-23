8b655b385beebc01b0c309232f3f04f84536d119
Robert Griffith <49240490+bibbycodes@users.noreply.github.com>
2021-01-23 16:45:44 +0000

    feat: add beforeEach to allow for sharing of request object between tests

--- tests/request.test.js
+++ tests/request.test.js
@@ -2,21 +2,23 @@ const { it, describe } = require('@jest/globals');
 const Request = require('../lib/request')
 
 describe('Request', () => {
+  
   describe('get', () => {
+    beforeEach(() => {
+      request = new Request()
+    })
+
     it('returns a promise', () => {
-      const request = new Request()
       expect(request.get('https://jsonplaceholder.typicode.com/todos/1').then instanceof Function).toBe(true)
     })
 
     it('rejects when passed an empty string', async () => {
-      const request = new Request()
       await expect(request.get('')).rejects.toEqual({
         error: "The Url is invalid"
       })
     })
 
     it('returns json data when fetching from a json endpoint', () => {
-      const request = new Request()
       return request.get('https://jsonplaceholder.typicode.com/todos/1')
         .then(data => {
           expect(typeof JSON.parse(data)).toBe('object')
@@ -24,14 +26,12 @@ describe('Request', () => {
     })
 
     it('has a status code of 200 on successful retrieval of data', () => {
-      const request = new Request()
       return request.get('https://jsonplaceholder.typicode.com/todos/1').then(() => {
         expect(request.statusCode).toBe(200)
       })
     })
 
     it('has a status code of 404 when fetching from an unknown url', () => {
-      const request = new Request()
       return request.get('https://www.gmail.com/hello').then(() => {
         expect(request.statusCode).toBe(404)
       })
