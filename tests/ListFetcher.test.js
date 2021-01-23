9652178312ecbfb40f4b4f252bee5f71af4086c2
Robert Griffith <49240490+bibbycodes@users.noreply.github.com>
2021-01-23 21:27:44 +0000

    feat: listFetcher rejects on empty array

--- tests/ListFetcher.test.js
+++ tests/ListFetcher.test.js
@@ -5,4 +5,10 @@ describe('listFetcher', () => {
   it('returns a promise', () => {
     expect(listFetcher(['http://localhost:3100/api/users']).then instanceof Function).toBe(true)
   })
+
+  it('rejects when passed an empty array', async () => {
+    await expect(listFetcher([])).rejects.toEqual({
+      error: "Please provide at least one url"
+    })
+  })
 })
