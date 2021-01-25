6b4040f3d1bce74052c9934d238f12394c130a8f
Robert Griffith <49240490+bibbycodes@users.noreply.github.com>
2021-01-25 18:06:24 +0000

    fix: remove duplicated tests

--- tests/validate.test.js
+++ tests/validate.test.js
@@ -27,20 +27,6 @@ describe('Validate', () => {
     });
   });
 
-  describe('isValidURL', () => {
-    it('returns true when passed a valid url', () => {
-      expect(Validate.isUrl('https://www.google.com')).toBe(true);
-    });
-
-    it('returns false when passed notvalid/ru', () => {
-      expect(Validate.isUrl('notvalid/ru')).toBe(false);
-    });
-
-    it('returns true when passed 127.0.0.1', () => {
-      expect(Validate.isUrl('https://127.0.0.1')).toBe(true);
-    });
-  });
-
   describe('isJsonData', () => {
     it('returns true when passed a json object', () => {
       expect(Validate.isJson(jsonResponse)).toBe(true);
